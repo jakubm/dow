79df816bc60d6771db60e56b0ba6d646425e76b0
Jakub Mišina <jakub.misina@gmail.com>
2021-08-02 09:16:45 +0200

    First working version

--- dow.go
+++ dow.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("dow - program for showing day of week for a date")
+	params := os.Args[1:]
+	if len(params) > 0 {
+		for _, v := range params {
+			t, _ := time.Parse("2006-01-02", v)
+			fmt.Println(v, t.Weekday())
+		}
+	} else {
+		fmt.Println("dow - program for showing day of week for a date")
+		fmt.Println("	Usage: dow YYYY-MM-DD [YYYY-MM-DD, ...]")
+	}
 }
